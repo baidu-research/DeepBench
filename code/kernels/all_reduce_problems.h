fc3741045a1a0c178a7a27ebd2b7e85be3810b01
Mostofa Patwary <patwarymostofa@baidu.com>
2017-11-21 16:53:30 -0800

    comments removed

--- code/kernels/all_reduce_problems.h
+++ code/kernels/all_reduce_problems.h
@@ -5,7 +5,6 @@
 #define _NUMBER_OF_KERNELS_ 7
 
 int64_t all_reduce_kernels_size[] = {100000, 3097600, 4194304, 6553600, 16777217, 38360000, 64500000};
-//int64_t all_reduce_kernels_repeat[] = {100, 100, 100, 100, 10, 10, 10};
 int64_t all_reduce_kernels_repeat[] = {10000, 10000, 10000, 10000, 1000, 1000, 1000};
 
 #endif
